36ec175c399ba63ef6150d19f9eff2cbc6902641
ephicz <42580612+ephicz@users.noreply.github.com>
2019-12-03 08:09:12 +0100

    commit navbar fix

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React from 'react';
-import logo from './Ephi dark - svg.svg';
 import logo2 from './Ephi dark - svg edited.svg';
 import './App.css';
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 function App() {
+    let stylesbody = {
+        padding: '0 5vw'
+    }
+    let stylesli = {
+        color: 'white',
+        padding: '0 0 0 3vw',
+        fontSize: '120%',
+    };
+    let stylesnav = {
+        padding: '1% 1% 0 1%'
+    };
     return (
         <div className="App">
-            <header className="App-header">
-                <Navbar variant="dark" expand="md" sticky="top" className="paddingNav">
-                        <Navbar.Brand href="#">
-                            <img src={logo2} className="App-logo" alt="logo"/>
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-                        <Navbar.Collapse>
-                            <Nav className="ml-auto">
-                                <Nav.Link href="#">Úvod</Nav.Link>
-                                <Nav.Link href="#">O nás</Nav.Link>
-                                <Nav.Link href="#">Portfolio</Nav.Link>
-                                <Nav.Link href="#">Kontakty</Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
+            <header className="App-header" style={stylesbody}>
+                <Navbar expand="md" sticky="top" className="paddingNav" style={stylesnav}>
+                    <Navbar.Brand href="#">
+                        <img src={logo2} className="App-logo" alt="logo"/>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse>
+                        <Nav className="ml-auto">
+                            <Nav.Link href="#" style={stylesli}>Úvod</Nav.Link>
+                            <Nav.Link href="#" style={stylesli}>O nás</Nav.Link>
+                            <Nav.Link href="#" style={stylesli}>Portfolio</Nav.Link>
+                            <Nav.Link href="#" style={stylesli}>Kontakty</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
-
+                <container>
+                    <Row>
+                        <Col>zaujmou</Col>
+                        <Col>pravo</Col>
+                    </Row>
+                </container>
             </header>
         </div>
     );
