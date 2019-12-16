80bd6227e87434e04a9139a87f75dcdff4cf8f39
ephicz <42580612+ephicz@users.noreply.github.com>
2019-12-16 15:04:53 +0100

    hoveronportfolio

--- src/components/Kosticka.js
+++ src/components/Kosticka.js
@@ -3,48 +3,19 @@ import Row from "react-bootstrap/Row";
 import Toast from "react-bootstrap/Toast";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import './KostickaStyle.scss';
 
 export default class Kosticka extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hovered: false,
-            clicked: false
-        };
-    }
 
     render() {
-        let imgStyle = {
-            width: '100%',
-            opacity: '50%'
-        };
-        let allStyle = {
-            width: '40vw',
-            padding: '0.5vw'
-        };
-        let hoveredStyle = {};
-        if (this.state.hovered){
-            hoveredStyle = {
-                width: '100%',
-                height: '100%',
-                backgroundColor: 'white'
-            };
-            imgStyle = {
-              display: 'none'
-            };
-        } else {
-            hoveredStyle = {
-            display: 'none'
-            };
-        }
         return (
-            <Card bg="dark" className="text-center">
+            <Card bg="dark" className="text-center kosticka">
                 <Card.Img
                     variant="top"
                     src={this.props.data.image} alt={this.props.data.jmeno}
-                    style={imgStyle}
+                    className="kostickaImg"
                 />
-                <Card.ImgOverlay>
+                <Card.ImgOverlay className="kostickaOverlay">
                     <Card.Title className="font-weight-bold">{this.props.data.jmeno}</Card.Title>
                     <Card.Text>{this.props.data.text}</Card.Text>
                     <Button variant="light" size="sm">Vice info</Button>
