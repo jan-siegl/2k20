5b6c68ae29cd85f0a73d816ddde75a5e6337caee
PeanutButte7 <adam.barta007@gmail.com>
2020-02-03 09:27:46 +0100

    Ready to merge

--- src/components/uvod.js
+++ src/components/uvod.js
@@ -9,12 +9,12 @@ export default class Uvod extends Component {
         return(
             <container id="uvod">
                 <Row className="mainRow justify-content-center">
-                    <Col sm={12} lg className="text-left my-auto">
+                    <Col sm={12} xl className="text-left my-auto">
                         <h1 className="nospacing">Tvoříme webovové stránky, které zaujmou</h1>
-                        <h2 className="nospacing">Ať už potřebujete webovou stránku nebo kompletní brand
+                        <h2 className="">Ať už potřebujete webovou stránku nebo kompletní brand
                             design, jsme tu pro vás.</h2>
                     </Col>
-                    <Col sm={12} lg className="my-auto">
+                    <Col sm={12} xl className="my-auto">
                         <img src={ilustrace1} className="img-fluid" alt="Ilustrace"/>
                     </Col>
                 </Row>
